d5ae2f08ee77f1d311957046cfbab6d40a769219
bill apiyo <billian177@gmail.com>
2022-06-12 00:31:32 +0300

    commit tree 53c8102cbc46f2d913b6e6a158f63071a2af426c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+superbowlWin = (array) => {
+    const win = array.find( array=> array.result === "W" );
+    return !!win ? win.year : undefined;
+  }
+
+
+////function superbowlWin(win){
+    //function win(array){
+      //  return array.find(result==="W");
+
+    //}
+//}
